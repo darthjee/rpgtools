1965b583c4a063094025452afdbb4da765546ae2
Fernando Favini <darthjee@gmail.com>
2015-04-01 19:51:32 -0300

    Add room#show route

--- app/controllers/chat/rooms_controller.rb
+++ app/controllers/chat/rooms_controller.rb
@@ -1,4 +1,17 @@
 class Chat::RoomsController < ApplicationController
   def show
+    render :show, { room: created_room }
+  end
+
+  private
+
+  def created_room
+    room
+  rescue ActiveRecord::RecordNotFound
+    Chat::Room.create key: params.require(:id)
+  end
+
+  def room
+    Chat::Room.find_by! key: params.require(:id)
   end
 end

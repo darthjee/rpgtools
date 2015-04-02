44feefc72c02c583e609b3aa9727a014dede1180
Fernando Favini <darthjee@gmail.com>
2015-04-02 16:13:29 -0300

    Use hellper to show message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def show_message(message)
+    %Q(
+      <p>
+        <span>#{message.session.nick}:</span>
+        #{message.text}
+      <p>
+      ).html_safe
+  end
 end
---
+++ app/helpers/chat/message_helper.rb
@@ -0,0 +1,2 @@
+module Chat::MessageHelper
+end

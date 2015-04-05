17e9c2d539843434adf410fd37fb37b6ed2e2374
Fernando Favini <darthjee@gmail.com>
2015-04-04 21:25:11 -0300

    Extract user login concern shared context

--- spec/support/shared_examples/login.rb
+++ spec/support/shared_examples/login.rb
@@ -24,3 +24,53 @@ shared_examples 'a controller that handles user login' do
     end
   end
 end
+
+shared_examples 'a controller that controls user session' do
+  let(:user) { users(:user1) }
+
+  before do
+    cookies.delete(:credentials)
+  end
+
+  describe 'require_logged' do
+    context 'when user is not logged' do
+      let(:redirect) { new_login_path(redirect_to: '/anonymous') }
+
+      it 'redirects to redirect_login_path' do
+        get :index
+        expect(response).to redirect_to(redirect)
+      end
+    end
+
+    context 'when user is logged' do
+      before do
+        controller.sign_in(user)
+      end
+
+      it 'does not redirect to redirect_login_path' do
+        get :index
+        expect(response).not_to be_a_redirect
+      end
+    end
+  end
+
+  describe 'login' do
+    context 'when user is not logged' do
+      it 'store the user email encrypted' do
+        controller.sign_in(user)
+        expect(cookies.signed[:credentials]).to eq(user.email)
+      end
+    end
+  end
+
+  describe 'logged_user' do
+    context 'user is not logged' do
+      it { expect(controller.logged_user).to be_nil }
+    end
+
+    context 'user is logged' do
+      before { controller.sign_in(user) }
+      it { expect(controller.logged_user).to eq(user) }
+    end
+  end
+end

5dd89ecb50b8ac9912ce0961c8836ed7066728df
Fernando Favini <darthjee@gmail.com>
2015-04-06 19:52:01 -0300

    Cache variables

--- app/controllers/concerns/chat/room_concern.rb
+++ app/controllers/concerns/chat/room_concern.rb
@@ -4,10 +4,10 @@ module Chat::RoomConcern
   private
 
   def room
-    Chat::Room.find_or_create_by key: room_key
+    @room ||= Chat::Room.find_or_create_by key: room_key
   end
 
   def room_key
-    params[:room_id] || params.require(:id)
+    @room_key ||= (params[:room_id] || params.require(:id))
   end
 end

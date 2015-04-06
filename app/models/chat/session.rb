e367c1bd41944ed89b612ea47126417f6fc5f716
Fernando Favini <darthjee@gmail.com>
2015-04-06 18:34:45 -0300

    Remove uneeded symbolize keys

--- app/models/chat/session.rb
+++ app/models/chat/session.rb
@@ -3,7 +3,6 @@ class Chat::Session < ActiveRecord::Base
   belongs_to :room
 
   def self.create_or_update(attributes)
-    attributes = attributes.deep_symbolize_keys
     nick = attributes.delete(:nick)
 
     find_or_create_by(attributes).tap do |s|

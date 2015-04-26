b87f967efb99492f65d27390be15134f7caf4cc9
Fernando Favini <darthjee@gmail.com>
2015-04-26 11:40:57 -0700

    Add validation shared examples

--- spec/models/chat/message_spec.rb
+++ spec/models/chat/message_spec.rb
@@ -2,12 +2,8 @@ require 'spec_helper'
 
 describe Chat::Message do
   describe :validations do
-    let(:message) { chat_messages(:first) }
+    let(:object) { chat_messages(:first) }
 
-    context 'when room has not been defined' do
-      before { message.room = nil }
-
-      it { expect(message).to be_invalid }
-    end
+    it_behaves_like 'object that has mandatory attributes', :room
   end
 end

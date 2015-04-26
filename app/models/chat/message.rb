bc8f9a83abf9f624c6bda2f01fd0dfee5b8501db
Fernando Favini <darthjee@gmail.com>
2015-04-26 11:36:13 -0700

    Add message basic validation

--- app/models/chat/message.rb
+++ app/models/chat/message.rb
@@ -2,4 +2,6 @@ class Chat::Message < ActiveRecord::Base
   belongs_to :room
   belongs_to :session
   belongs_to :target, class: Chat::Session
+
+  validates_presence_of :room
 end
--- spec/models/chat/message_spec.rb
+++ spec/models/chat/message_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe Chat::Message do
+  describe :validations do
+    let(:message) { chat_messages(:first) }
+
+    context 'when room has not been defined' do
+      before { message.room = nil }
+
+      it { expect(message).to be_invalid }
+    end
+  end
 end
